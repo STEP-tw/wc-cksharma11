38c6b310b6a86d6cc42de6eba54b5dc3c3b9857c
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-26 22:48:20 +0530

    Options order is now maintained

--- src/format.js
+++ src/format.js
@@ -5,15 +5,21 @@ const formatWithOneOption = function(result, [option]) {
   return ['', count, result.filename].join(TAB);
 };
 
-const formatWithTwoOption = function(result, [firstOption, secondOption]) {
-  const count1 = result[firstOption];
-  const count2 = result[secondOption];
+const formatWithTwoOption = function(result, options) {
+  const sortedOptions = sortOptions(options);
+  const count1 = result[sortedOptions[0]];
+  const count2 = result[sortedOptions[1]];
   return ['', count1, count2, result.filename].join(TAB);
 };
 
 const formatWithThreeOption = function(result, options) {
   const { lineCount, wordCount, charCount, filename } = result;
   return ['', lineCount, wordCount, charCount, filename].join(TAB);
+};
+
+const sortOptions = function(options) {
+  const sortedOptions = ['lineCount', 'wordCount', 'charCount'];
+  return sortedOptions.filter(option => options.includes(option));
 };
 
 const getFormatter = function(options) {
