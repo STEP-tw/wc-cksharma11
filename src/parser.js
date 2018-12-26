d39cf34fde7e2b29f907f458645502c7ad6fe97b
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-26 23:39:55 +0530

    Extract getDefaultOptions and extractCombinedOptions()

--- src/parser.js
+++ src/parser.js
@@ -14,15 +14,23 @@ const joinAndSplitBySpace = function(text) {
 };
 
 const filterOptions = function(args) {
-  let options = args.filter(arg => arg.startsWith(HYPHEN));
+  const options = args.filter(arg => arg.startsWith(HYPHEN));
   if (options.length == 1) {
-    options = options[0].split(EMPTYSTRING).slice(1);
-    return options.map(option => HYPHEN + option);
+    return extractCombinedOptions(options);
   }
   if (options.length == 0) {
-    return ['-l', '-w', '-c'];
+    return getDefaultOptions();
   }
   return options;
+};
+
+const extractCombinedOptions = function(options) {
+  const extractedOptions = options[0].split(EMPTYSTRING).slice(1);
+  return extractedOptions.map(option => HYPHEN + option);
+};
+
+const getDefaultOptions = function() {
+  return ['-l', '-w', '-c'];
 };
 
 const filterFilenames = function(args) {
