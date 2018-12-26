c980d8fd7a55060af3d7ba03d7f3a0854226bcab
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-26 17:03:53 +0530

    Add test cases for parse()

---
+++ test/parserTest.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const { parse } = require('../src/parser.js');
+
+describe('parse', () => {
+  it('should give option and filename when one option provided', () => {
+    const args = ['-l', 'file'];
+    const expectedOutput = { filenames: ['file'], options: ['lineCount'] };
+    const actualOutput = parse(args);
+
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should give option and filename when two option provided', () => {
+    const args = ['-l', '-w', 'file'];
+    const expectedOutput = {
+      filenames: ['file'],
+      options: ['lineCount', 'wordCount']
+    };
+    const actualOutput = parse(args);
+
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should give option and filename when three option provided', () => {
+    const args = ['-l', '-w', '-c', 'file'];
+    const expectedOutput = {
+      filenames: ['file'],
+      options: ['lineCount', 'wordCount', 'charCount']
+    };
+    const actualOutput = parse(args);
+
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should give option and filename when no option provided', () => {
+    const args = ['file'];
+    const expectedOutput = { filenames: ['file'], options: [] };
+    const actualOutput = parse(args);
+
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+});
