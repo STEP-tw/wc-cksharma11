38b39e84bfacdd0873e2b026b411be22bdddb70c
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-27 15:18:14 +0530

    Extract getAllCounts() from wc()

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,4 @@
-const { NEWLINE, EMPTYSTRING } = require('./constants.js');
+const { NEWLINE, EMPTYSTRING, UTF8 } = require('./constants.js');
 
 const getLineCount = function(content) {
   return content.split(NEWLINE).length - 1;
@@ -13,20 +13,21 @@ const getCharCount = function(content) {
 };
 
 const getFileContent = function(filenames, fs) {
-  return filenames.map(filename => [
-    fs.readFileSync(filename, 'utf8'),
-    filename
-  ]);
+  return filenames.map(filename => [fs.readFileSync(filename, UTF8), filename]);
+};
+
+const getAllCounts = function(content) {
+  const lineCount = getLineCount(content);
+  const wordCount = getWordCount(content);
+  const charCount = getCharCount(content);
+  return { lineCount, wordCount, charCount };
 };
 
 const wc = function(filenames, fs) {
   const content = getFileContent(filenames, fs);
   return content.map(([content, filename]) => {
-    const lineCount = getLineCount(content);
-    const wordCount = getWordCount(content);
-    const charCount = getCharCount(content);
-
-    return { lineCount, wordCount, charCount, filename: filename };
+    const { lineCount, wordCount, charCount } = getAllCounts(content);
+    return { lineCount, wordCount, charCount, filename };
   });
 };
 
