ca2f7f1cb059bcfd1382f21f8f4b7dbe80128052
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-12-27 15:03:46 +0530

    Refactor format(): used TAB outside

--- src/format.js
+++ src/format.js
@@ -2,10 +2,8 @@ const { TAB, EMPTYSTRING } = require('./constants.js');
 
 const format = function(result, options) {
   const sortedOptions = sortOptions(options);
-  const counts = sortedOptions.map(option => {
-    return TAB + result[option];
-  });
-  return counts.join(EMPTYSTRING) + TAB + result.filename;
+  const counts = sortedOptions.map(option => result[option]);
+  return TAB + counts.join(TAB) + TAB + result.filename;
 };
 
 const singleFileFormatter = function(result, options) {
